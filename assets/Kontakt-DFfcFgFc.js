b8795b3c037a619eb1dbeb6ee551bb3a85b60c71
Leonhard Weiler <Laser_Leo@hotmail.com>
2025-12-30 13:06:50 +0100

    Auto deploy

---
+++ assets/Kontakt-DFfcFgFc.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CfxAS4lu.js";function r(){return t.jsx(t.Fragment,{children:t.jsx("h1",{children:"Kontakt"})})}export{r as default};
--- assets/Kontakt-z5iRZLyM.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dy-Qk1Hv.js";function r(){return t.jsx(t.Fragment,{children:t.jsx("h1",{children:"Kontakt"})})}export{r as default};
